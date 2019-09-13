f42c74d952ac0bf5b9f6ee8380c1ec45e55dcc22
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-13 11:32:11 -0500

    validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+    validates :email, presence: true, length: { minimum: 8, maximum: 250 }
 end
