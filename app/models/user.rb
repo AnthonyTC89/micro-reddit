be5836033d6d3a44ba746fe75be6a43cc202f853
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-16 11:15:19 -0500

    User uniqueness validation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :comments
-    validates :name, presence: true, length: { minimum: 4, maximum: 50 }
-    validates :email, presence: true, length: { minimum: 8, maximum: 250 }
+    validates :name, presence: true, length: { minimum: 4, maximum: 50 },
+            uniqueness: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { minimum: 8, maximum: 250 },
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false} 
 end
