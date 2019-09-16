5993902cac93c03c209c7cb9043aefb9d2accf48
stickler-ci <support@stickler-ci.com>
2019-09-16 16:15:13 +0000

    Fixing style errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    validates :name, presence: true, length: { minimum: 4, maximum: 50 },
-            uniqueness: true
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { minimum: 8, maximum: 250 },
-            format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false} 
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true, length: { minimum: 4, maximum: 50 },
+                   uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { minimum: 8, maximum: 250 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
